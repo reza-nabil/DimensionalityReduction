30b2157faff2fa4ef4996a76c807c0d5f64391c7
reza-nabil <mohamadrezanabil@yahoo.com.com>
2021-08-17 19:49:23 +0430

    before predict test data

--- LDAvsPCA.py
+++ LDAvsPCA.py
@@ -8,31 +8,38 @@ from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.model_selection import train_test_split
 
 # dataset loading
-x, y = load_iris(return_X_y=True)
+iris = load_iris()
+x_train, x_test, y_train, y_test = train_test_split(iris.data,
+                                                    iris.target,
+                                                    test_size = 0.3,
+                                                    random_state = 20)
+
 
 #feature extraction --> with PCA and LDA
 num_components = 2
 
 pca_obj = PCA(n_components = num_components)
-X_new_pca = pca_obj.fit_transform(x)
+X_new_pca = pca_obj.fit_transform(x_train)
 
 lda_obj = LDA(n_components = num_components)
-X_new_lda = lda_obj.fit_transform(x,y)
+X_new_lda = lda_obj.fit_transform(x_train, y_train)
 
 
-# model fiting/classification --> with KNN
+# model fiting (train model) / classification --> with KNN
 num_neighbors = 5
+
 knn_obj_pca = KNN(n_neighbors=num_neighbors)
-knn_obj_pca.fit(X_new_pca, y)
+knn_obj_pca.fit(X_new_pca, y_train)
 y_pca = knn_obj_pca.predict(X_new_pca)
 
 knn_obj_lda = KNN(n_neighbors=num_neighbors)
-knn_obj_lda.fit(X_new_lda, y)
+knn_obj_lda.fit(X_new_lda, y_train)
 y_lda = knn_obj_lda.predict(X_new_lda)
 
-
+# testing model (predict)
 # show Result
-print('LDA performance: ', np.sum((y == y_lda)/len(y)))
-print('PCA performance: ', np.sum((y == y_pca)/len(y)))
+print('LDA performance: ', np.sum((y_train == y_lda)/len(y_train)))
+print('PCA performance: ', np.sum((y_train == y_pca)/len(y_train)))
